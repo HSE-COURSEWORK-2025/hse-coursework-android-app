38cda4db17fe84c1be4b640634815a128ac7894e
Igor <igor@oleg.ru>
2025-05-26 12:24:23 +0300

    kinda fixed exception

--- app/src/main/java/com/example/healthconnectsample/presentation/BaseApplication.kt
+++ app/src/main/java/com/example/healthconnectsample/presentation/BaseApplication.kt
@@ -1,25 +1,31 @@
-/*
- * Copyright 2024 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.healthconnectsample.presentation
 
 import android.app.Application
+import android.util.Log
 import com.example.healthconnectsample.data.HealthConnectManager
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.SupervisorJob
 
 class BaseApplication : Application() {
-    val healthConnectManager by lazy {
-        HealthConnectManager(this)
+    lateinit var healthConnectManager: HealthConnectManager
+        private set
+
+    override fun onCreate() {
+        super.onCreate()
+        healthConnectManager = HealthConnectManager(this)
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e("GlobalError", "Uncaught exception in thread ${thread.name}", throwable)
+            // приложение не упадёт — просто залогируем
+        }
     }
+
+    /**
+     * Общий [SupervisorJob] и [CoroutineExceptionHandler], которые можно
+     * подключать к своим scope’ам, чтобы игнорировать ошибки в дочерних корутинах.
+     */
+    val appCoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.e("GlobalError", "Unhandled coroutine exception", throwable)
+    }
+    val appSupervisorJob = SupervisorJob()
 }
